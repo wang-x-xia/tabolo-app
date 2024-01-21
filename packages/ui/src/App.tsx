c4d473f8121d58cdece4fa6705f1c6ac862a3e84
Wang Xia <wang_x_xia@163.com>
2024-01-21 16:22:46 +0800

    Add tailwind css.

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,22 +1,11 @@
-import {Layout} from "antd";
 import {PrepareLocal} from "./json/PrepareLocal.tsx";
 import {View} from "./view/View.tsx";
 
 export function App() {
-    return (
-        <Layout>
-            <Layout.Content>
-                <PrepareLocal>
-                    <View/>
-                </PrepareLocal>
-            </Layout.Content>
-            <Layout.Footer>
-                ©{new Date().getFullYear()}
-                <a href="https://github.com/wang-x-xia/tabolo-app">Tabolo</a>
-                All Rights Reserved.
-            </Layout.Footer>
-        </Layout>
-    )
+    return <PrepareLocal>
+        <View/>
+    </PrepareLocal>
+
 }
 
 
