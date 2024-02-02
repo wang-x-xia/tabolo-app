7a57328532e3a28f9bd1d1efc82bdf372fd5496f
Wang Xia <wang_x_xia@163.com>
2024-02-02 23:25:40 +0800

    Rewrite RelationshipEdit

--- ui/edit/RelationshipEdit.tsx
+++ ui/edit/RelationshipEdit.tsx
@@ -1,7 +1,9 @@
-import {Button, Input, Space} from "antd";
+import {Button, Textarea, TextInput} from "flowbite-react";
 import {useContext, useEffect, useState} from "react";
 import {type GraphRelationship} from "../../core";
 import {NodeIdCell} from "../cell/NodeCell.tsx";
+import {RelationshipCell} from "../cell/RelationshipCell.tsx";
+import {useMenuItem} from "../view/menu.tsx";
 import {ViewHandlerContext} from "../view/view.ts";
 import {GraphEditContext} from "./graph-edit.ts";
 import {TypeSelect} from "./TypeSelect.tsx";
@@ -30,6 +32,7 @@ export function RelationshipEdit({data}: {
         history.back()
     }
 
+    const deleteItem = useMenuItem("Delete Relationship", <Button onClick={remove}>Delete</Button>);
 
     async function copy() {
         const relationship = await graphEdit.copyRelationship(data.id);
@@ -39,34 +42,52 @@ export function RelationshipEdit({data}: {
         });
     }
 
+    const copyItem = useMenuItem("Copy Relationship", <Button onClick={copy}>Copy</Button>);
+
     async function save() {
         setLocal(await graphEdit.editRelationshipProperty(data.id, JSON.parse(property)))
     }
 
+    const savePropertyItem = useMenuItem("Save Relationship Property", <Button onClick={save}>Save</Button>);
 
     function reset() {
         setProperty(JSON.stringify(local.properties))
     }
 
+    const resetPropertyItem = useMenuItem("Reset Relationship Property", <Button onClick={reset}>Reset</Button>);
+
     return <>
-        <div>Relationship ID</div>
-        <Input disabled value={local.id}/>
-        <div>Action</div>
-        <Space.Compact block>
-            <Button onClick={copy}>Copy</Button>
-            <Button onClick={remove}>Delete</Button>
-        </Space.Compact>
-        <div>Type</div>
-        <TypeSelect type={local.type} source="Relationship" onChange={setType}/>
-        <div>Start Node</div>
-        <NodeIdCell data={data.startNodeId}/>
-        <div>End Node</div>
-        <NodeIdCell data={data.endNodeId}/>
-        <div>Property</div>
-        <Input.TextArea rows={20} value={property} onChange={e => setProperty(e.target.value)}/>
-        <Space.Compact block>
-            <Button onClick={save}>Save</Button>
-            <Button onClick={reset}>Reset</Button>
-        </Space.Compact>
+        {deleteItem}
+        {copyItem}
+        {savePropertyItem}
+        {resetPropertyItem}
+        <div className="flex space-x-2">
+            <div className="flex flex-col items-center p-4 min-w-64 space-y-2">
+                <label className="text-lg self-start">Start Node</label>
+                <div className="flex flex-col items-center w-full p-6 bg-gray-50">
+                    <NodeIdCell data={data.startNodeId}/>
+                </div>
+                <label className="text-lg self-start">Overview</label>
+                <div className="flex flex-col items-center w-full p-6 bg-gray-100">
+                    <RelationshipCell data={local}/>
+                </div>
+                <label className="text-lg self-start">End Node</label>
+                <div className="flex flex-col items-center w-full p-6 bg-gray-50">
+                    <NodeIdCell data={data.endNodeId}/>
+                </div>
+            </div>
+            <div className="flex flex-col grow p-4 space-y-4">
+                <label className="text-lg">Relationship ID</label>
+                <TextInput className="max-w-96" disabled value={local.id}/>
+                <label className="text-lg">Type</label>
+                <div className="max-w-48">
+                    <TypeSelect type={local.type} source="Relationship" onChange={setType}/>
+                </div>
+                <label className="text-lg">Property</label>
+                <Textarea rows={property.split("\n").length}
+                          value={property}
+                          onChange={e => setProperty(e.target.value)}/>
+            </div>
+        </div>
     </>
 }
