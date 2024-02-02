b9b256b46ccb0c1d040c1b0069b207016e663d2d
Wang Xia <wang_x_xia@163.com>
2024-02-02 23:14:11 +0800

    Rewrite RelationshipCell

--- ui/cell/RelationshipCell.tsx
+++ ui/cell/RelationshipCell.tsx
@@ -1,5 +1,4 @@
-import {MoreOutlined} from "@ant-design/icons"
-import {Button, Popover, Space} from "antd"
+import {Dropdown} from "flowbite-react";
 import {useContext} from "react"
 import type {GraphRelationship} from "../../core"
 import {ViewHandlerContext} from "../view/view"
@@ -16,10 +15,17 @@ export function RelationshipCell({data}: {
         })
     }
 
-    return <Popover title={`${data.type} Properties`} content={JSON.stringify(data.properties, null, 2)}>
-        <Space.Compact block>
-            <Button>{`<${data.type}>`}</Button>
-            <Button icon={<MoreOutlined/>} onClick={editRelationship}/>
-        </Space.Compact>
-    </Popover>
+    return <div className="flex flex-col items-center
+        rounded-lg border border-gray-200 bg-white shadow-md
+        dark:border-gray-700 dark:bg-gray-800
+        w-fit max-w-64 p-2 gap-1">
+        <div className="self-end">
+            <Dropdown inline label="">
+                <Dropdown.Item>
+                    <a onClick={editRelationship}>Edit</a>
+                </Dropdown.Item>
+            </Dropdown>
+        </div>
+        <h5 className="font-bold text-xs">{`[${data.type}]`}</h5>
+    </div>
 }
